6d3a6b2f2d7f251838b9908a8d2de0d45e80351d
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-03-23 12:14:59 -0400

    feat: Add more TODO markers that explain how to implement functions in person.py.

--- objectprocessor/objectprocessor/person.py
+++ objectprocessor/objectprocessor/person.py
@@ -40,22 +40,13 @@ class Person:
         self, name: str, country: str, phone_number: str, job: str, email: str
     ) -> None:
         """Define the constructor for a person."""
-        self.name = name
-        self.country = country
-        self.phone_number = phone_number
-        self.job = job
-        self.email = email
+        # TODO: implement all of the assignment statemnets for the constructor
 
     def __repr__(self) -> str:
         """Return a textual representation of the person."""
-        return f"{self.name} is a {self.job} who lives in {self.country}. You can call this person at {self.phone_number} and email them at {self.email}"
+        # TODO: create a textual representation for a person object
 
     def create_list(self) -> List[str]:
         """Create a list of strings representing the person."""
-        details = []
-        details.append(self.name)
-        details.append(self.country)
-        details.append(self.phone_number)
-        details.append(self.job)
-        details.append(self.email)
-        return details
+        # TODO: implement a function that will create a list from the attributes of a person
+        return []
